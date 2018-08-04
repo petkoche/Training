85f6fa4261b147eb9c63dd59a3a05d13e408d9b9
petkoche <petkoche90@gmail.com>
2018-08-04 16:26:16 +0300

    TapeEqulibrium

--- CodilityTasks/TapeEquilibrium/Program.cs
+++ CodilityTasks/TapeEquilibrium/Program.cs
@@ -11,16 +11,36 @@ namespace TapeEquilibrium
         static void Main(string[] args)
         {
             var A = new int[] { 3, 1, 2, 4, 3 };
+            Console.WriteLine(Solution(A));
         }
 
         public static int Solution(int[] A)
         {
+            int totalSum = 0;
 
+            var results = new HashSet<int>();
 
+            foreach (var item in A)
+            {
+                totalSum += item;
+            }
 
-            return 1;
+            int prev = A[0];
+            int next = totalSum - prev;
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                results.Add(ReturnModulus(prev, next));
+                prev += A[i];
+                next = totalSum - prev;
+            }
+
+            return results.Min();
         }
 
-        public static int
+        public static int ReturnModulus(int start, int end)
+        {
+            return Math.Abs(start - end);
+        }
     }
 }
