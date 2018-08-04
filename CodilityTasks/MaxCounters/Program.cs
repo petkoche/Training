f4ffd84eea81e733697a011a4de402de47c70f5e
petkoche <petkoche90@gmail.com>
2018-08-04 19:59:41 +0300

    MaxCounters

---
+++ CodilityTasks/MaxCounters/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MaxCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 4, 4, 6, 1, 4, 4};
+            int n = 5;
+            var res = Solution(n, a);
+
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            a = new int[] { 3, 4, 4, 6, 1, 4, 4, 6, 6, 6, 6 };
+            n = 5;
+            res = Solution(n, a);
+
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            a = new int[] { 1, 1, 0, 0, 0, 0, 0, 6, 6, 6, 6 };
+            n = 5;
+            res = Solution(n, a);
+
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static int[] Solution(int n, int[] a)
+        {
+            int currentMax = 0;
+            int lastMax = 0;
+            int[] counters = new int[n];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] >= 1 && a[i] <= n)
+                {
+                    counters[a[i] - 1] = Math.Max(counters[a[i] - 1], lastMax);
+                    counters[a[i] - 1] += 1;
+                    currentMax = Math.Max(currentMax, counters[a[i] - 1]);
+                }
+                else if (a[i] == n + 1)
+                {
+                    lastMax = currentMax;
+                }
+            }
+
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = Math.Max(counters[i], lastMax);
+            }
+
+            return counters;
+        }
+    }
+}
