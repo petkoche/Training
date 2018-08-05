ffe41e401b970e8179ddb73ea824c9be1107f5fb
petkoche <petkoche90@gmail.com>
2018-08-05 22:44:22 +0300

    Triangle

---
+++ CodilityTasks/Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] { 10, 2, 5, 1, 8, 20};
+            Console.WriteLine(Solution(a));
+        }
+
+        public static int Solution(int[] a)
+        {
+            Array.Sort(a);
+            for (int i = 0; i < a.Length - 2; i++)
+            {
+                if (a[i] + a[i + 1] > a[i + 2])                    
+                {
+                    return 1;
+                }
+
+                if (a[i] == a[i + 2] && a[i + 2] == a[i + 1] && a[i] == Int32.MaxValue)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
