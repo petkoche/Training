3ce56e449b39c05963b7fcabb50c81a642baecd9
petkoche <petkoche90@gmail.com>
2018-08-05 21:32:32 +0300

    MaxProduct

---
+++ CodilityTasks/MaxProductOfThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaxProductOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] { -3, 1, 2, -2, 5, 6};
+            Console.WriteLine(Solution(a));
+        }
+
+        public static int Solution(int[] a)
+        {
+            Array.Sort(a);
+
+            int firstCase = a[0] * a[1] * a[a.Length - 1];
+            int secondCase = a[a.Length - 1] * a[a.Length - 2] * a[a.Length - 3];
+
+            return (firstCase > secondCase) ? firstCase : secondCase;
+        }
+    }
+}
