36374d6e61be737d7d195bf8e683f2913cc95524
petkoche <petkoche90@gmail.com>
2018-08-05 17:50:39 +0300

    CountDiv

---
+++ CodilityTasks/CountDiv/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountDiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(6, 11, 2));
+            Console.WriteLine(Solution(0, 0, 2));
+            Console.WriteLine(Solution(0, int.MaxValue, 2));
+        }
+
+        public static int Solution(int a, int b, int k)
+        {
+            int bRes = b / k;
+            int aRes = a / k;
+
+            int res = (a % k == 0) ? bRes - aRes + 1 : bRes - aRes;
+
+            return res;
+        }
+    }
+}
