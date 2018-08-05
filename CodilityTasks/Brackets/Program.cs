e9f4c7afcf7e625b4d013b35943d901a8a1e06f6
petkoche <petkoche90@gmail.com>
2018-08-05 23:04:01 +0300

    Nesting

---
+++ CodilityTasks/Brackets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Nesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets = "(()(())())";
+            Console.WriteLine(Solution(brackets));
+        }
+
+        public static int Solution(string brackets)
+        {
+            int openingCount = 0;
+            int closingCount = 0;
+
+            for (int i = 0; i < brackets.Length; i++)
+            {
+                if (brackets[i] == '(')
+                {
+                    openingCount++;
+                }
+                else
+                {
+                    closingCount++;
+                }
+
+                if(openingCount < closingCount)
+                {
+                    return 0;
+                }
+            }
+
+            return (openingCount == closingCount) ? 1 : 0;
+        }
+    }
+}
