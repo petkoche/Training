8662fd9fffd1be2b3293e5442dac8fb174f8d7c0
petkoche <petkoche90@gmail.com>
2018-08-06 00:36:37 +0300

    Brackets

---
+++ CodilityTasks/Bracket/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bracket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nesting = "{[()()]}";
+            Console.WriteLine(Solution(nesting));
+            nesting = "([)()]";
+            Console.WriteLine(Solution(nesting));
+            nesting = "{{{";
+            Console.WriteLine(Solution(nesting));
+            nesting = "({{({}[]{})}}[]{})";
+            Console.WriteLine(Solution(nesting));
+        }
+
+        public static int Solution(string S)
+        {
+            Dictionary<string, string> matched = new Dictionary<string, string>();
+            matched.Add("]", "[");
+            matched.Add("}", "{");
+            matched.Add(")", "(");
+
+            List<string> pushElement = new List<string>();
+            pushElement.Add("[");
+            pushElement.Add("{");
+            pushElement.Add("(");
+
+            Stack stack = new Stack();
+
+            foreach (char c in S)
+            {
+                if (pushElement.Contains(c.ToString()))
+                {
+                    stack.Push(c.ToString());
+                }                    
+                else if (stack.Count == 0)
+                {
+                    return 0;
+                }                    
+                else if (!stack.Pop().Equals(matched[c.ToString()]))
+                {
+                    return 0;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return 1;
+            }               
+
+            return 0;
+        }
+    }
+}
