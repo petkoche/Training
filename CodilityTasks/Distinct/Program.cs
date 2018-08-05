541aea51784f2354a0dcec8ce032403c019501b3
petkoche <petkoche90@gmail.com>
2018-08-05 03:17:30 +0300

    Distinct

---
+++ CodilityTasks/Distinct/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 2, 1, 1, 2, 3, 1 };
+            Console.WriteLine(Solution(a));
+        }
+
+        public static int Solution(int[] a)
+        {
+            var res = new HashSet<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                res.Add(a[i]);
+            }
+
+            return res.Count();
+        }
+    }
+}
